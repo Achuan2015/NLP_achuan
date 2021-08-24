b3d6dd62cecc14d73ec37851ab1f857a2aeac7d6
hrtps-chuan <hrtps@hrtpsdeMacBook-Pro.local>
2021-08-24 14:14:12 +0800

    updaate

--- classification/text-classification-bert/src/engine.py
+++ classification/text-classification-bert/src/engine.py
@@ -23,6 +23,7 @@ def eval_fn(dataloader, model, device):
 
     eval_loss = 0.0
     fin_output = []
+    fin_target = []
     for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
         input_ids, token_type_id, attention_mask, target = data['input_ids'].to(device), data['token_type_id'].to(device), \
             data['attention_mask'].to(device), data['targets'].to(device)
@@ -30,6 +31,7 @@ def eval_fn(dataloader, model, device):
         loss = outputs[0]
         logits = outputs[1]
         fin_output.extend(logits.cpu().detach().numpy().tolist())
+        fin_target.extend(target.view(-1).cpu().detach().numpy().tolist())
         eval_loss += loss.item() * input_ids.size(0)
     eval_loss = eval_loss / len(dataloader.sampler)
-    return eval_loss, fin_output
+    return eval_loss, fin_output, fin_target
