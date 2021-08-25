801cbd406c9c8db2d685e983d0d6550b42503122
hrtps-chuan <hrtps@hrtpsdeMacBook-Pro.local>
2021-08-25 18:07:35 +0800

    update

--- classification/text-classification-bert/src/model.py
+++ classification/text-classification-bert/src/model.py
@@ -25,7 +25,7 @@ class BertForSequenceClassification(nn.Module):
         super(BertForSequenceClassification, self).__init__()
         self.config = config
         self.bert = BertModel.from_pretrained(config.model_path)
-        self.dropout = config.hidden_dropout_prob
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
     
     def forward(self, input_ids, attention_mask, token_type_ids):
