2dd2041f5463d2f6839010fda88ed14d3cb8798a
hrtps-chuan <hrtps@hrtpsdeMacBook-Pro.local>
2021-08-30 15:09:21 +0800

    update

--- semantic_similairy/text-semantic-similarity-bert/src/train_nezha.py
+++ semantic_similairy/text-semantic-similarity-bert/src/train_nezha.py
@@ -10,8 +10,8 @@ from transformers import BertForSequenceClassification
 from transformers import get_linear_schedule_with_warmup
 
 from dataset import SiameseDataset, CrossEncodeDataset
-from model import BertForSiameseNetwork
-from engine import train_fn, eval_fn
+from model import BertCNNForClassification
+from engine import train_mse_fn, eval_mse_fn, train_fn, eval_fn
 import config_nezha as config
 
 
@@ -97,6 +97,79 @@ def run():
             best_accuracy = eval_accu
             model.module.save_pretrained(config.output_path)
 
+def run_second():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    path_train = "../data/50_kmeans_train_data.csv"
+    dfs = pd.read_csv(path_train, sep="\t")
+    df_train, df_valid = model_selection.train_test_split(
+        dfs,
+        test_size=0.1,
+        random_state=42,
+        stratify=dfs.label.values
+    )
+
+    dataset_train = CrossEncodeDataset(
+        querys=df_train['query'].values,
+        candidates=df_train['candidate'].values,
+        labels=df_train['label'].values
+    )
+
+    dataset_valid = CrossEncodeDataset(
+        querys=df_valid['query'].values,
+        candidates=df_valid['candidate'].values,
+        labels=df_valid['label'].values
+    )
+
+    dataloader_train = DataLoader(
+        dataset_train,
+        batch_size=config.batch_size,
+        shuffle=True
+    )
+
+    dataloader_valid = DataLoader(
+        dataset_valid,
+        batch_size=config.batch_size,
+        shuffle=False
+    )
+
+    # 初始化 BERT model
+    config_bert = BertConfig.from_pretrained(config.model_path)
+    model = BertCNNForClassification(config_bert, config)
+
+    model = nn.DataParallel(model, device_ids=[0,1]).to(device)
+    # 构造分组参数优化器
+    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+    param_optimizer = list(model.named_parameters())
+    optimizer_parameters = [
+        {
+            'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+            'weight_decay': config.weight_decay
+        },
+        {
+            'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+            'weight_decay': 0
+        }
+    ]
+    optimizer = optim.AdamW(optimizer_parameters, lr=config.learning_rate)
+    
+    # 设置训练的 scheduler
+    num_training_step = (len(df_train) / config.batch_size) * config.epochs
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps=0,
+        num_training_steps= num_training_step
+    )
+    # loop the epochs
+    best_accuracy = 0.9
+    for epoch in range(config.epochs):
+        train_loss = train_mse_fn(dataloader_train, model, device, optimizer, scheduler)
+        eval_loss, eval_accu = eval_mse_fn(dataloader_valid, model, device)
+        print(f'epoch:{epoch+1} | train_loss:{train_loss} | eval_loss:{eval_loss} | accuracy:{eval_accu}')
+        if eval_accu > best_accuracy:
+            best_accuracy = eval_accu
+            model.module.save_pretrained(config.output_path)
+
 
 if __name__ == "__main__":
-    run()
+    run_second()
