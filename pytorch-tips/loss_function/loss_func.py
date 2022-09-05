617a8a8e9065b2c9ccd24c35a75c218665922592
hrtps-chuan <hrtps@hrtpsdeMacBook-Pro.local>
2022-09-05 16:15:24 +0800

    update

--- pytorch-tips/loss_function/loss_func.py
+++ pytorch-tips/loss_function/loss_func.py
@@ -30,9 +30,21 @@ class Focal_Loss(nn.Module):
         return loss.mean()
         
 
-def focal_loss(pred, target, alpha=0.25, gamma=2):
+
+class Contrastive_Loss(nn.Module):
+
     '''
-     alpha * (1 - f(x)) ** gamma * f(x)
+    contrastive loss function: Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    如果是正样本就是 欧几里得距离，如果是负样本，则要求至少达到 m 的距离（其中m 是常量）
     '''
-    bce_loos = F.binary_cross_entropy_with_logits(pred, target, reduction='None')
     
+    def __init__(self, margin):
+        super(Contrastive_Loss, self).__init__()
+        self.margin = margin
+
+    def forward(self, target1, target2, label):
+        # 计算欧几里得距离
+        euclidean_distance = F.pairwise_distance(target1, target2)
+        # 当 label=0 时，loss 的值就是 euclidean distance 的值，如果 label=1 时，loss 的值为 max(0, m - euclidean_distance)
+        contrastive_loss =  (1 - label) * torch.pow(euclidean_distance, 2) + label * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0), 2)
+        return contrastive_loss.mean()
